061729b4c8e3113109f6dd04b965fce510eaabc7
learnICT <kibrisli7@gmail.com>
2021-12-19 23:02:59 +0000

    initial commit

---
+++ post/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import requests
+import json
+
+# Create your views here.
+def index(request):
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+
+    time = data["time"]["updated"]
+    usd = data["bpi"]["USD"]["rate"]
+    gbp = data["bpi"]["GBP"]["rate"]
+    eur = data["bpi"]["EUR"]["rate"]
+    context = {'usd':usd, 'gbp':gbp, 'eur':eur, 'time':time}
+    return render(request, 'post/index.html', context)
