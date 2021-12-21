149d0d31a83d6d37dd20e4f510653a2874c3b381
learnICT <kibrisli7@gmail.com>
2021-12-21 11:10:13 +0000

    update to secret key settings

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # htmx_guide_2-Periodic-Refreshing
+
+read the associated blog post @ https://medium.com/@bitFez/django-and-htmx-part-2-periodic-refreshing-768cc059a06d
+
+## To recreate this:
+- add a file named `.env` in the root folder
+- create a variable like `SECRET_KEY='replace_this_part_with_secret_key'` in the `.env` file
+- generate a secret key from here https://miniwebtool.com/django-secret-key-generator/
+- Open a command prompt in the root directory and run `git clone https://github.com/bitFez/htmx_guide_2-periodic_refreshing.git`
+- finally using the command prompt run `pip install -r requirements.txt`
--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import os
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,8 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rkzq842^&oz8q3ei4c^wsv(6=nm!r9!)405xe15+qlgxs(q-ho'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
